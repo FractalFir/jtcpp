dd7d51ab213625c266c8baeb4fc2e61a51b5c730
FractalFir <fractalfirdev@gmail.com>
2023-07-18 13:34:12 +0200

    Fixed build process.

--- build.rs
+++ build.rs
@@ -1,62 +1,76 @@
 use std::io::Result;
 use std::process::{Child, Command};
-fn handle_command(command: Result<Child>) {
-    let result = command.expect("failed to execute process").wait().unwrap();
-    let code = result.code().expect("command termianted by signal!");
-    //assert!(code == 0 || code == 128);
+fn clone_bdwgc(){
+    let path = std::env::var_os("OUT_DIR").expect("OUT_DIR is not set").to_owned();
+    let result = Command::new("git")
+            .current_dir(path)
+            .arg("clone")
+            .arg("--depth")
+            .arg("1")
+            .arg("https://github.com/ivmai/bdwgc")
+            .output()
+            .expect("failed to execute process");
+    if(result.stderr.len() > 0){
+        let message = std::str::from_utf8(&result.stderr).expect("Error message not UTF-8");
+        if !(message.contains("already exists and is not an empty directory") || message.contains("Cloning into '")){
+            panic!("error:{}",message);
+        }
+    }
 }
-fn clone_bdwgc() -> Result<Child> {
-    let path = std::env::var_os("OUT_DIR")
-        .expect("OUT_DIR is not set")
-        .to_owned();
-    Command::new("git")
-        .current_dir(path)
-        .arg("clone")
-        .arg("--depth")
-        .arg("1")
-        .arg("https://github.com/ivmai/bdwgc")
-        .spawn()
-}
-fn clone_libatomic_ops() -> Result<Child> {
-    let mut path = std::env::var_os("OUT_DIR")
-        .expect("OUT_DIR is not set")
-        .to_owned();
+fn clone_libatomic_ops(){
+    let mut path =std::env::var_os("OUT_DIR").expect("OUT_DIR is not set").to_owned();
     //This is NOT how docs describe this ought to work!
     path.push("/bdwgc/");
-    Command::new("git")
-        .current_dir(path)
-        .arg("clone")
-        .arg("--depth")
-        .arg("1")
-        .arg("https://github.com/ivmai/libatomic_ops")
-        .spawn()
+    let result = Command::new("git")
+            .current_dir(path)
+            .arg("clone")
+            .arg("--depth")
+            .arg("1")
+            .arg("https://github.com/ivmai/libatomic_ops")
+            .output()
+            .expect("failed to execute process");
+    if(result.stderr.len() > 0){
+        let message = std::str::from_utf8(&result.stderr).expect("Error message not UTF-8");
+        if !(message.contains("already exists and is not an empty directory") || message.contains("Cloning into '")){
+            panic!("error:{}",message);
+        }
+    }
 }
-fn configure_cmake<P: AsRef<std::path::Path>>(compile_path: P) -> Result<Child> {
-    Command::new("cmake")
+fn configure_cmake<P: AsRef<std::path::Path>>(compile_path: P) {
+    let result = Command::new("cmake")
         .current_dir(compile_path)
         .arg("-Denable_cplusplus=ON")
         .arg("..")
-        .spawn()
+        .output()
+        .unwrap();
+    if(result.stderr.len() > 0){
+        let message = std::str::from_utf8(&result.stderr).expect("Error message not UTF-8");
+        if !(message.contains("Explicit GC_INIT() calls may be required.")){
+            panic!("error:{}",message);
+        }
+    }
 }
-fn cmake_build<P: AsRef<std::path::Path>>(compile_path: P) -> Result<Child> {
-    Command::new("cmake")
+fn cmake_build<P: AsRef<std::path::Path>>(compile_path: P) {
+    let result =  Command::new("cmake")
         .current_dir(compile_path)
         .arg("--build")
         .arg(".")
-        .spawn()
+        .output()
+        .unwrap();
+    if(result.stderr.len() > 0){
+        let message = std::str::from_utf8(&result.stderr).expect("Error message not UTF-8");
+        panic!("error:{}",message);
+            
+    }
 }
 fn main() {
-    let mut commands = Vec::with_capacity(10);
-    commands.push(clone_bdwgc());
-    commands.push(clone_libatomic_ops());
+    clone_bdwgc();
+    clone_libatomic_ops();
     let mut compile_path = std::env::var_os("OUT_DIR")
         .expect("OUT_DIR is not set")
         .to_owned();
     compile_path.push("/bdwgc/out");
     std::fs::create_dir_all(&compile_path).unwrap();
-    commands.push(configure_cmake(&compile_path));
-    commands.push(cmake_build(&compile_path));
-    commands
-        .into_iter()
-        .for_each(|command| handle_command(command));
+    configure_cmake(&compile_path);
+    cmake_build(&compile_path);
 }
